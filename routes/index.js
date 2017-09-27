993e4f204df4d071dc4e16481165d540418b13c1
Konstadina Laiou <konstadina27@gmail.com>
2017-09-27 23:13:03 +0300

    Api and testing setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const router = express.Router(); //handles http trafic
+
+const router = express.Router(); // handles http trafic
 
 /* GET home page. */
-router.get('/', (req, res, next) => { 
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'MusicList Alpha' });
 });
 
 module.exports = router;
+
