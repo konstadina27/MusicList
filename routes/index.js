1c801f901ac839a59c342c2d41a45c8e84c42d2d
Konstadina Laiou <konstadina27@gmail.com>
2017-09-27 20:18:32 +0300

    initial express scaffolding

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router(); //handles http trafic
+
+/* GET home page. */
+router.get('/', (req, res, next) => { 
+  res.render('index', { title: 'MusicList Alpha' });
+});
+
+module.exports = router;
